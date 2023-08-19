6043e5c4bb9991fd6bd0165ee1973c704c83b5c1
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-19 10:58:42 +0900

    Add: 가운데 글자 가져오기 [lv-1]

--- 연습문제/week_05/가운데 글자 가져오기/Answer.js
+++ 연습문제/week_05/가운데 글자 가져오기/Answer.js
@@ -1,12 +1,10 @@
 function solution(s) {
-    for(let i = 0; i < s.length; i++) {
-        let answer = '';
-        // 짝수일 때
-        if(s.length % 2 == 0) {
-            answer += s[parseInt((s.length / 2) - 1)] + s[parseInt(s.length / 2)]
-        } else {
-            answer += s[parseInt(s.length / 2)]
-        }
-        return answer
+    let answer = ''; 
+
+    if (s.length % 2 == 0) {
+        answer += s[parseInt((s.length / 2) - 1)] + s[parseInt(s.length / 2)];
+    } else {
+        answer += s[parseInt(s.length / 2)];
     }
+    return answer; 
 }
