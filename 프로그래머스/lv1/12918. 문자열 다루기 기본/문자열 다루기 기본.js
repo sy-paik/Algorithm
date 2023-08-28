90018898fdcd01d09e29393acd093d0669da2704
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-08-28 18:43:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -2,6 +2,5 @@ function solution(s) {
     if (s.length !== 4 && s.length !== 6) {
         return false;
     }
-    
-    return Array.from(s).every(char => char >= '0' && char <= '9');
+    return Array.from(s).every(char => !isNaN(Number(char)));
 }
