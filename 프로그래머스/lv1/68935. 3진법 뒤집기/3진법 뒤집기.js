fdf9078028abf13b19afac9015a765781513d042
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-09-05 22:57:26 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -2,11 +2,11 @@ function solution(n) {
   let array = [];
     let answer = 0;
     while(n !== 0) {
-        array.unshift(n % 3);
+        array.push(n % 3);
         n = Math.floor(n / 3);
     }
     for (let i = 0; i < array.length; i++) {
-        answer += array[i] * Math.pow(3, i);
+        answer += array[i] * Math.pow(3, array.length - i -1);
     }
     return answer;
 }
