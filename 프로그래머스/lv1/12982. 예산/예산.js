fc717e981b047e7d0ff8c91b416f731c188b7862
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-09-05 23:25:57 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,14 +1,15 @@
 function solution(d, budget) {
-    var answer = 0, sum = 0;
+    let sum = 0;
+    let answer = 0;
     d.sort((a,b) => a - b);
-
     for(let i = 0; i < d.length; i++){
         answer++;
         sum += d[i]
 
-        if(sum > budget)
-            answer--;
+        if(sum > budget) {
+            answer --;
+            break;
+        }
     }
-  
     return answer;
 }
