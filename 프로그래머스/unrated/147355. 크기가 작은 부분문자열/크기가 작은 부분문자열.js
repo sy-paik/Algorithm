f0da64a886f4e82cebe7d810b3a8129f9c1d6b62
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-09-11 18:19:03 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,17 +1,12 @@
 function solution(t, p) {
-  const result = [];
-
-  for (let i = 0; i <= t.length - p.length; i++) {
-    const substring = t.slice(i, i + p.length);
-
-    // 부분 문자열을 숫자로 변환하여 비교
-    const substringNum = parseInt(substring);
+  let result = [];
+  for(let i = 0; i <= t.length - p.length; i++) {
+    const v = t.slice(i, i+p.length);
+    const vNum = parseInt(v);
     const pNum = parseInt(p);
-
-    if (substringNum <= pNum) {
-      result.push(substring);
+    if(vNum <= pNum) {
+    result.push(vNum);
     }
   }
-
-  return result.length;
+  return result.length
 }
