7c90de1df30e7e1f5a0d61348f601a850ebd8386
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-09-12 12:40:54 +0900

    [level 1] Title: 시저 암호, Time: 1.08 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,29 +1,5 @@
+
 function solution(s, n) {
-  // 알파벳 패턴 배열
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  // 시저 암호화 함수
-  const caesarCipher = (char, pattern) => {
-    const index = pattern.indexOf(char);
-    if (index !== -1) {
-      const newIndex = (index + n) % pattern.length;
-      return pattern[newIndex];
-    }
-    return char;
-  };
-
-  let result = "";
-
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if (/[a-z]/.test(char)) {
-      // 소문자 알파벳
-      result += caesarCipher(char, alphabet);
-    } else {
-      result += caesarCipher(char, upperAlphabet);
-    }
-  }
-
-  return result;
+    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+    return s.split('').map(e => chars[chars.indexOf(e)+n]).join('');
 }
