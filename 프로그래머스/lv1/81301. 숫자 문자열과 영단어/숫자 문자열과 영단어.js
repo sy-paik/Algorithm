7e85c1af2a920a9fe65409ca12545d26df8bd979
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-09-16 23:16:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,23 @@
+function solution(s) {
+    const numberObj = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    };
+
+    for (const word in numberObj) {
+        s = s.split(word).join(numberObj[word]);
+    }
+    return parseInt(s);
+}
+
+
+
+
