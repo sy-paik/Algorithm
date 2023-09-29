1ce645dbff70af886fc982c84ca73cb3795ed17b
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-29 22:51:44 +0900

    Update: 불필요한 파일 정리

--- Lv-0/032-외계어사전.js
+++
@@ -1,5 +0,0 @@
-function solution(spell, dic) {
-    const sortedDic = dic.map(word => word.split('').sort().join(''));
-    const sortedSpell = spell.sort().join('');
-    return sortedDic.includes(sortedSpell) ? 1 : 2;
-}
--- Lv-0/035-삼각형의 완성조건(2).js
+++
@@ -1,3 +0,0 @@
-function solution(sides) {
-    return sides.reduce((acc, curr) => acc + curr) - Math.max(...sides) + Math.min(...sides) - 1;
-}
