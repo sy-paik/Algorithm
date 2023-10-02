bd874c6904b26a1072f91c888b2978e33cd15cd4
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-10-02 11:28:06 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,10 @@
+function solution(food) {
+  let answer = "";
+  for (let i = 0; i < food.length; i++) {
+    if (food[i] === 1) {
+      continue;
+    }
+    answer += String(i).repeat(Math.floor(food[i] / 2));
+  }
+  return answer + "0" + [...answer].reverse().join("");
+}
