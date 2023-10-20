006dd19570f36d8a0b1a55aa48b649dbd53075d8
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-10-20 10:34:59 +0900

    [level 1] Title: 모의고사, Time: 5.71 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,23 +1,19 @@
 function solution(answers) {
-    const one = [1, 2, 3, 4, 5];
+    const one = [1, 2, 3, 4 ,5];
     const two = [2, 1, 2, 3, 2, 4, 2, 5];
     const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     let score = [0, 0, 0];
-
-    for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === one[i % one.length]) score[0]++;
-        if (answers[i] === two[i % two.length]) score[1]++;
-        if (answers[i] === three[i % three.length]) score[2]++;
+    for(let i = 0; i < answers.length; i++) {
+        if (answers[i] === one[i % one.length]) score[0]++
+        if (answers[i] === two[i % two.length]) score[1]++
+        if (answers[i] === three[i % three.length]) score[2]++
     }
-
-    const maxScore = Math.max(...score);
-    const answer = [];
-
-    for (let i = 0; i < score.length; i++) {
-        if (score[i] === maxScore) {
+    let answer = [];
+    const max = Math.max(...score);
+    for(let i = 0; i < 3; i++) {
+        if(score[i] === max) {
             answer.push(i + 1);
         }
     }
-
-    return answer;
+    return answer
 }
