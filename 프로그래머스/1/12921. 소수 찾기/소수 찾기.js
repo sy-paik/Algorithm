7cb45b6747f6f392b5ee3f5ce5557991df91c7a7
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-10-23 00:44:09 +0900

    [level 1] Title: 소수 찾기, Time: 16.59 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -2,7 +2,6 @@ function solution(n) {
     if (n < 2) return 0; 
 
     let isPrime = new Array(n + 1).fill(true); 
-    isPrime[0] = isPrime[1] = false; 
 
     for (let i = 2; i * i <= n; i++) {
         if (isPrime[i]) {
