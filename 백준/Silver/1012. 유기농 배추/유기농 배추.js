46d2cf1efb4fd8e789e306e6e8e04f55ed115414
sy-paik <80268199+sy-paik@users.noreply.github.com>
2023-11-20 22:02:55 +0900

    [Silver II] Title: 유기농 배추, Time: 196 ms, Memory: 12664 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -3,22 +3,16 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
-function bfs(x, y, field, visited) {
-  const queue = [[x, y]];
+function dfs(x, y, field, visited) {
   visited[y][x] = true;
 
-  while (queue.length > 0) {
-    const [cx, cy] = queue.shift();
+  for (let i = 0; i < 4; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
 
-    for (let i = 0; i < 4; i++) {
-      const nx = cx + dx[i];
-      const ny = cy + dy[i];
-
-      if (nx >= 0 && nx < field[0].length && ny >= 0 && ny < field.length) {
-        if (field[ny][nx] === 1 && !visited[ny][nx]) {
-          queue.push([nx, ny]);
-          visited[ny][nx] = true;
-        }
+    if (nx >= 0 && nx < field[0].length && ny >= 0 && ny < field.length) {
+      if (field[ny][nx] === 1 && !visited[ny][nx]) {
+        dfs(nx, ny, field, visited);
       }
     }
   }
@@ -43,18 +37,16 @@ for (let i = 0; i < T; i++) {
     currentIndex++;
   }
 
-  let wormCount = 0;
-
   for (let y = 0; y < N; y++) {
     for (let x = 0; x < M; x++) {
       if (field[y][x] === 1 && !visited[y][x]) {
-        bfs(x, y, field, visited);
-        wormCount++;
+        dfs(x, y, field, visited);
+        count++;
       }
     }
   }
 
-  results.push(wormCount);
+  results.push(count);
 }
 
 results.forEach((result) => {
