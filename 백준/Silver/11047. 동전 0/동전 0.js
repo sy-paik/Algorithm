547c8507b54a8bc709bebcc678a011e673098867
sy-paik <80268199+sy-paik@users.noreply.github.com>
2024-03-15 16:02:59 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,15 +1,18 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [N, K] = input.shift().split(' ').map(Number);
+const [N, M] = input.shift().split(' ');
 
 const coins = input.map(Number).reverse(); // 내림차순 정렬
 
 let answer = 0;
-let remainingAmount = K; 
+let count = 0;
+
+let remainingAmount = parseInt(M);
 
 for (let i = 0; i < N; i++) {
     if (coins[i] <= remainingAmount) {
-        answer += Math.floor(remainingAmount / coins[i]);
-        remainingAmount %= coins[i];
+        count = Math.floor(remainingAmount / coins[i]);
+        answer += count;
+        remainingAmount -= coins[i] * count;
     }
 }
 
